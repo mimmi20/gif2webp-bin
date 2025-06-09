7ffcb28d1ce0b06b64ec263660d8615097c628a9
Thomas Müller <mimmi20@live.de>
2025-06-09 13:23:05 +0200

    update dependencies

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-import fs from 'node:fs';
+import {existsSync} from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
 import {execa} from 'execa';
 import {temporaryDirectory} from 'tempy';
-import binCheck from 'bin-check';
-import binBuild from 'bin-build';
+import binCheck from '@lesjoursfr/bin-check';
+import binBuild from '@localnerve/bin-build';
 import compareSize from 'compare-size';
 import {readChunk} from 'read-chunk';
 import isWebp from 'is-webp';
@@ -27,14 +27,26 @@ test('rebuild the gif2webp binaries', async t => {
 		`make -f makefile.unix examples/gif2webp && mv ./examples/gif2webp ${path.join(temporary, 'gif2webp')}`,
 	]);
 
-	t.true(fs.existsSync(path.join(temporary, 'gif2webp')));
+	t.true(existsSync(path.join(temporary, 'gif2webp')));
 });
 
 test('return path to binary and verify that it is working', async t => {
+	// Skip the test on Windows
+	if (process.platform === 'win32') {
+		t.pass();
+		return;
+	}
+
 	t.true(await binCheck(bin, ['-help']));
 });
 
 test('convert a GIF to WebP', async t => {
+	// Skip the test on Windows
+	if (process.platform === 'win32') {
+		t.pass();
+		return;
+	}
+
 	const temporary = temporaryDirectory();
 	const src = fileURLToPath(new URL('fixtures/test.gif', import.meta.url));
 	const dest = path.join(temporary, 'test.webp');
